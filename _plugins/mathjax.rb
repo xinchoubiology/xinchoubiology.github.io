35f6fa891857d3f4d63bf8925073be5b594b4ffd
xinchoubiology <xinchoubiology@gmail.com>
2014-07-31 02:28:39 +0800

    add lambda calculus's new post

--- _plugins/mathjax.rb
+++ _plugins/mathjax.rb
@@ -15,7 +15,7 @@ class HTMLwithAlbino < Redcarpet::Render::HTML
     if code[0] == "$" && code[-1] == "$"
       code.gsub!(/^\$/,'')
       code.gsub!(/\$$/,'')
-      "<span><script type=\"math/tex\">#{code}</script></span>"
+      "<script type=\"math/tex\">#{code}</script>"
     else
       "<code>#{code}</code>"
     end
